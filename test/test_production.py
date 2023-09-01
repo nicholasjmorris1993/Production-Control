5ece06630df244b888e49f9ff4cd6c44da117d14
Nick <129078463+nicholasjmorris1993@users.noreply.github.com>
2023-09-01 17:58:18 -0400

    Add files via upload

--- test/test_production.py
+++ test/test_production.py
@@ -1,1 +1,12 @@
+import pandas as pd
+import sys
+sys.path.append("/home/nick/Production-Control/src")
+from production import rate
 
+
+data = pd.read_csv("/home/nick/Production-Control/test/tasks.csv")
+
+assembly = rate(data)
+
+print(assembly.cycle_times)
+print(f"Expected Hourly Rate: {assembly.simulation['Hourly Rate'].mean()}")
